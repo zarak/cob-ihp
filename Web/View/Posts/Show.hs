7f8d53992658f0566992ad217759e1a6c7520cb6
Zarak <7254237+zarak@users.noreply.github.com>
2021-01-04 11:00:23 +0500

    Fix html formatting

--- Web/View/Posts/Show.hs
+++ Web/View/Posts/Show.hs
@@ -18,22 +18,16 @@ instance View ShowView where
 
         <div>{get #body post |> renderMarkdown}</div>
 
-
         <div>
-
             <section class="rounded-b-lg  mt-4 ">
-              
-        <a href={NewCommentAction (get #id post)}>
-            <button class="py-2 px-4 w-full bg-red-400 text-lg text-white shadow-md rounded-sm ">Add Comment </button>
-        </a>
-      <div id="task-comments" class="pt-4">
-
-        <div>{forEach (get #comments post) renderComment2}</div>
-
-          </div>
-        </section>
-
-      </div>
+                <a href={NewCommentAction (get #id post)}>
+                    <button class="py-2 px-4 w-full bg-red-400 text-lg text-white shadow-md rounded-sm ">Add Comment </button>
+                </a>
+                <div id="task-comments" class="pt-4">
+                    <div>{forEach (get #comments post) renderComment2}</div>
+                </div>
+            </section>
+        </div>
     |]
         where renderUpvote post =
                 case currentUserOrNothing of 
@@ -65,7 +59,7 @@ renderUpvoteHtml post = [hsx|
 
 
 renderComment2 comment = [hsx|
-        <!--     comment-->
+<!--     comment-->
 <div class="bg-white rounded-lg p-3  flex flex-col justify-center items-center md:items-start shadow-lg mb-4">
   <div class="flex flex-row justify-center mr-2">
     <img alt="avatar" width="48" height="48" class="rounded-full w-10 h-10 mr-4 shadow-lg mb-4" src="https://cdn1.iconfinder.com/data/icons/technology-devices-2/100/Profile-512.png">
@@ -74,17 +68,6 @@ renderComment2 comment = [hsx|
 
 
     <p style="width: 90%" class="text-gray-600 text-lg text-center md:text-left ">Hi good morning will it be the entire house. </p>
-
-</div>
-<!--  comment end--><!--     comment-->
-<div class="bg-white rounded-lg p-3  flex flex-col justify-center items-center md:items-start shadow-lg mb-4">
-  <div class="flex flex-row justify-center mr-2">
-    <img alt="avatar" width="48" height="48" class="rounded-full w-10 h-10 mr-4 shadow-lg mb-4" src="https://cdn1.iconfinder.com/data/icons/technology-devices-2/100/Profile-512.png">
-    <h3 class="text-purple-600 font-semibold text-lg text-center md:text-left ">@Tim Motti</h3>
-  </div>
-
-
-    <p style="width: 90%" class="text-gray-600 text-lg text-center md:text-left "><span class="text-purple-600 font-semibold">@Shanel</span> Hello. Yes, the entire exterior, including the walls. </p>
 
 </div>
 <!--  comment end-->
