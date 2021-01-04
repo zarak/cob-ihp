4877da8400e79c6e8e0463158ba8d575a8728846
Zarak <7254237+zarak@users.noreply.github.com>
2021-01-04 12:21:12 +0500

    Make shadow in comments more subtle

--- Web/View/Posts/Show.hs
+++ Web/View/Posts/Show.hs
@@ -40,9 +40,9 @@ renderUpvoteHtml post = [hsx|
 
 renderComment comment = [hsx|
 <!--     comment-->
-<div class="bg-white rounded-lg p-3  flex flex-col justify-center items-center md:items-start shadow-lg mb-4">
+<div class="bg-white rounded-lg p-3  flex flex-col justify-center items-center md:items-start shadow mb-4">
   <div class="flex flex-row justify-center mr-2">
-    <!--<img alt="avatar" width="48" height="48" class="rounded-full w-10 h-10 mr-4 shadow-lg mb-4" src="https://cdn1.iconfinder.com/data/icons/technology-devices-2/100/Profile-512.png">-->
+    <!--<img alt="avatar" width="48" height="48" class="rounded-full w-10 h-10 mr-4 shadow mb-4" src="https://cdn1.iconfinder.com/data/icons/technology-devices-2/100/Profile-512.png">-->
     <h3 class="text-red-600 font-semibold text-center md:text-left ">@{get
         #author comment}</h3>
   </div>
