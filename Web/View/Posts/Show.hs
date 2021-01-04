fac7e7d68526e982189017dd4a25024dfbd7628b
Zarak <7254237+zarak@users.noreply.github.com>
2021-01-04 11:12:17 +0500

    Styling for comments

--- Web/View/Posts/Show.hs
+++ Web/View/Posts/Show.hs
@@ -21,7 +21,7 @@ instance View ShowView where
         <div>
             <section class="rounded-b-lg  mt-4 ">
                 <a href={NewCommentAction (get #id post)}>
-                    <button class="py-2 px-4 w-full bg-red-400 text-lg text-white shadow-md rounded-sm ">Add Comment </button>
+                    <button class="py-2 px-4 md:w-auto bg-red-400 text-red-50 shadow-md rounded-sm ">Add Comment </button>
                 </a>
                 <div id="task-comments" class="pt-4">
                     <div>{forEach (get #comments post) renderComment2}</div>
@@ -62,13 +62,19 @@ renderComment2 comment = [hsx|
 <!--     comment-->
 <div class="bg-white rounded-lg p-3  flex flex-col justify-center items-center md:items-start shadow-lg mb-4">
   <div class="flex flex-row justify-center mr-2">
-    <img alt="avatar" width="48" height="48" class="rounded-full w-10 h-10 mr-4 shadow-lg mb-4" src="https://cdn1.iconfinder.com/data/icons/technology-devices-2/100/Profile-512.png">
-    <h3 class="text-purple-600 font-semibold text-lg text-center md:text-left ">@Shanel</h3>
+    <!--<img alt="avatar" width="48" height="48" class="rounded-full w-10 h-10 mr-4 shadow-lg mb-4" src="https://cdn1.iconfinder.com/data/icons/technology-devices-2/100/Profile-512.png">-->
+    <h3 class="text-red-600 font-semibold text-center md:text-left ">@{get
+        #author comment}</h3>
   </div>
 
+  <p style="width: 90%" class="text-gray-700 text-center md:text-left ">
+      {get #body comment}
+  </p>
 
-    <p style="width: 90%" class="text-gray-600 text-lg text-center md:text-left ">Hi good morning will it be the entire house. </p>
-
+  <div class="modify flex-col justify-center items-center text-gray-500">
+      <a class="text-secondary mr-4" href={EditCommentAction (get #id comment)}>Edit</a>
+      <a class="text-secondary js-delete" href={DeleteCommentAction (get #id comment)}>Delete</a>
+  </div>
 </div>
 <!--  comment end-->
     |]
