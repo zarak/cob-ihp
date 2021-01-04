cb0d5826742b74c8d07fcff29f67ac51a68d90f9
Zarak <7254237+zarak@users.noreply.github.com>
2021-01-04 10:46:46 +0500

    Remove breadcrumb

--- Web/View/Posts/Show.hs
+++ Web/View/Posts/Show.hs
@@ -10,12 +10,6 @@ data ShowView = ShowView
 
 instance View ShowView where
     html ShowView { .. } = [hsx|
-        <nav>
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href={PostsAction}>Posts</a></li>
-                <li class="breadcrumb-item active">Show Post</li>
-            </ol>
-        </nav>
         <h1>{get #title post}</h1>
         <p>Author: {get #firstName author <> " " <> get #lastName author}</p>
         <p>{get #createdAt post |> timeAgo}</p>
