6175cdc343bf48f47e4cbfbade189d7b31052eca
Zarak <7254237+zarak@users.noreply.github.com>
2021-01-10 16:43:26 +0500

    Use toxic function instead of obscene.
    Closing #14 as the model is done

--- Web/View/Posts/Show.hs
+++ Web/View/Posts/Show.hs
@@ -89,7 +89,7 @@ renderPost post upvotes hasVoted l = [hsx|
                         <div class="flex justify-between items-center"><span
                              class="font-light text-gray-600">{get #createdAt post |> timeAgo}</span><a href="#"
                                 class="px-2 py-1 bg-gray-600 text-gray-100
-                                font-bold rounded hover:bg-gray-500">toxic {getScore l obscene}</a>
+                                font-bold rounded hover:bg-gray-500">toxic {getScore l toxic}</a>
                         </div>
                         <div class="mt-2">
                             <!--<a href="#" class="text-2xl-->
