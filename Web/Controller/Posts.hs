7e8c6395a8ee3a0e892edaf70e513b4b94b08fc3
Zarak <7254237+zarak@users.noreply.github.com>
2021-01-16 19:53:32 +0500

    Break string literals into multiple lines

--- Web/Controller/Posts.hs
+++ Web/Controller/Posts.hs
@@ -19,7 +19,9 @@ toxicThreshold = 0.3
 
 instance Controller PostsController where
     action PostsAction = do
-        numPosts :: Int <- sqlQueryScalar "select count(posts.*) from posts inner join predictions on posts.id = post_id where toxic > 0.3" ()
+        numPosts :: Int <- sqlQueryScalar "select count(posts.*) from posts \
+                                          \inner join predictions on posts.id = post_id \
+                                          \where toxic > 0.3" ()
 
         let page = paramOrError "page"
             validPage = case page of
@@ -29,7 +31,10 @@ instance Controller PostsController where
             (_, currentPage, pageSize, totalPages, _, _, _, _, pages) = 
                 paginate (fromIntegral numPosts) validPage 10 20
 
-        toxicPosts :: [Post] <- sqlQuery "select posts.* from posts inner join predictions on posts.id = post_id where toxic > 0.3 order by created_at desc limit ? OFFSET ?" [pageSize, (currentPage - 1) * pageSize]
+        toxicPosts :: [Post] <- sqlQuery "select posts.* from posts \
+                                         \inner join predictions on posts.id = post_id \
+                                         \where toxic > 0.3 order by created_at desc \
+                                         \limit ? OFFSET ?" [pageSize, (currentPage - 1) * pageSize]
 
         posts :: [Include "predictions" Post] <- collectionFetchRelated #predictions toxicPosts
 
