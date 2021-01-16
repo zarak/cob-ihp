0ea12bf786829288c4488550769f4060211bcc1e
Zarak <7254237+zarak@users.noreply.github.com>
2021-01-16 13:49:04 +0500

    Remove breadcrumbs in show user view

--- Web/View/Users/Show.hs
+++ Web/View/Users/Show.hs
@@ -5,12 +5,6 @@ data ShowView = ShowView { user :: User }
 
 instance View ShowView where
     html ShowView { .. } = [hsx|
-        <nav>
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href={UsersAction}>Users</a></li>
-                <li class="breadcrumb-item active">User Profile</li>
-            </ol>
-        </nav>
         <h1>Show User</h1>
         <p>First name: {get #firstName user}</p>
         <p>Last name: {get #lastName user}</p>
