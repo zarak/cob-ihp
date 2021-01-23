3fb42005a62ca1d72e7fb00633e031626ee31da2
Zarak <7254237+zarak@users.noreply.github.com>
2021-01-23 12:28:22 +0500

    Click on username for show user view

--- Admin/View/Users/Index.hs
+++ Admin/View/Users/Index.hs
@@ -14,37 +14,36 @@ instance View IndexView where
 <div class="container mx-auto px-4 sm:px-6 lg:px-8">
   <!-- Content goes here -->
 
-<div class="flex flex-col">
-  <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-      <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-        <table class="min-w-full divide-y divide-gray-200">
-          <thead class="bg-gray-50">
-            <tr>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Name
-              </th>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Email
-              </th>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Role
-              </th>
-              <th scope="col" class="relative px-6 py-3">
-                <span class="sr-only">Edit</span>
-              </th>
-            </tr>
-          </thead>
+  <div class="flex flex-col">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+          <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+              <tr>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Name
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Email
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Role
+                </th>
+                <th scope="col" class="relative px-6 py-3">
+                  <span class="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
 
-
-          <tbody>
-            {forEach (zip [1..] users) renderUser}
-          </tbody>
-        </table>
+            <tbody>
+              {forEach (zip [1..] users) renderUser}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
-</div>
 
 </div>
     |]
@@ -56,7 +55,9 @@ renderUser (i, user) = [hsx|
     where evenRow = [hsx|
             <tr class="bg-gray-50">
               <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                {get #firstName user <> " " <> get #lastName user}
+                <a href={ShowUserAction (get #id user)} class="text-indigo-600 hover:text-indigo-900">
+                    {get #firstName user <> " " <> get #lastName user}
+                </a>
               </td>
               <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                 {get #email user}
@@ -65,7 +66,6 @@ renderUser (i, user) = [hsx|
                 User
               </td>
               <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                <a href={ShowUserAction (get #id user)} class="text-indigo-600 hover:text-indigo-900">Show</a>
                 <a href={EditUserAction (get #id user)} class="mx-4 text-indigo-600 hover:text-indigo-900">Edit</a>
                 <a href={DeleteUserAction (get #id user)} class="text-indigo-600 hover:text-indigo-900">Delete</a>
               </td>
@@ -75,7 +75,9 @@ renderUser (i, user) = [hsx|
           oddRow = [hsx|
             <tr class="bg-white">
               <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                {get #firstName user <> " " <> get #lastName user}
+                <a href={ShowUserAction (get #id user)} class="text-indigo-600 hover:text-indigo-900">
+                    {get #firstName user <> " " <> get #lastName user}
+                </a>
               </td>
               <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                 {get #email user}
@@ -84,7 +86,6 @@ renderUser (i, user) = [hsx|
                 User
               </td>
               <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                <a href={ShowUserAction (get #id user)} class="text-indigo-600 hover:text-indigo-900">Show</a>
                 <a href={EditUserAction (get #id user)} class="mx-4 text-indigo-600 hover:text-indigo-900">Edit</a>
                 <a href={DeleteUserAction (get #id user)} class="text-indigo-600 hover:text-indigo-900">Delete</a>
               </td>
