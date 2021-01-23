6862321e33eac8f07607dc1e6c8ef80ec3bc109c
Zarak <7254237+zarak@users.noreply.github.com>
2021-01-23 19:02:02 +0500

    Remove create user button

--- Admin/View/Users/Index.hs
+++ Admin/View/Users/Index.hs
@@ -5,12 +5,6 @@ data IndexView = IndexView { users :: [User] }
 
 instance View IndexView where
     html IndexView { .. } = [hsx|
-<a href={pathTo NewUserAction}>
-    <button type="button" class="mb-4 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-      Create user
-    </button>
-</a>
-
 <!-- This example requires Tailwind CSS v2.0+ -->
   <div class="flex flex-col">
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
