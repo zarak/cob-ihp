a37c44d53737d03429dc3152cfb37cf40c123bb9
Zarak <7254237+zarak@users.noreply.github.com>
2021-01-23 11:14:01 +0500

    Add x-cloak

--- Application/Helper/View.hs
+++ Application/Helper/View.hs
@@ -109,6 +109,7 @@ navbar2 = [hsx|
             -->
             <div
                 x-show="profileDropdown"
+                x-cloak
                 x-transition:enter="transition ease-out duration-100"
                 x-transition:enter-start="transform opacity-0 scale-95"
                 x-transition:enter-end="transform opacity-100 scale-100"
