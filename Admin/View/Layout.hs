7fa7414ff34e5e32c2560c830b0f2b943551397c
Zarak <7254237+zarak@users.noreply.github.com>
2021-01-23 14:56:14 +0500

    Add admin links for mobile menu

--- Admin/View/Layout.hs
+++ Admin/View/Layout.hs
@@ -64,7 +64,7 @@ navbar = [hsx|
 
             Menu open: "hidden", Menu closed: "block"
           -->
-          <svg x-cloak x-bind:class="{'hidden': mobileMenu, 'block': !mobileMenu}" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+          <svg x-bind:class="{'hidden': mobileMenu, 'block': !mobileMenu}" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
           </svg>
           <!-- Icon when menu is open. -->
@@ -149,10 +149,7 @@ navbar = [hsx|
         <div class="flex-shrink-0">
           <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
         </div>
-        <div class="ml-3">
-          <div class="text-base font-medium text-white">Tom Cook</div>
-          <div class="text-sm font-medium text-gray-400">tom@example.com</div>
-        </div>
+        {mobileMenu}
         <button class="ml-auto flex-shrink-0 bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
           <span class="sr-only">View notifications</span>
           <!-- Heroicon name: bell -->
@@ -166,6 +163,7 @@ navbar = [hsx|
 </nav>
     |]
         where profileDropdown = maybe [hsx||] adminLinks currentAdminOrNothing
+              mobileMenu = maybe [hsx||] adminMobileLinks currentAdminOrNothing
 
 adminLinks :: Admin -> Html
 adminLinks user = [hsx|
@@ -174,6 +172,15 @@ adminLinks user = [hsx|
       <a href={DeleteSessionAction} class="js-delete js-delete-no-confirm block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Sign out</a>
 |]
 
+adminMobileLinks :: Admin -> Html
+adminMobileLinks user = [hsx|
+        <a href={ShowAdminAction (get #id user)}>
+        <div class="ml-3">
+          <div class="text-base font-medium text-white">Admin</div>
+          <div class="text-sm font-medium text-gray-400">{get #email user}</div>
+        </div>
+        </a>
+    |]
 
 stylesheets :: Html
 stylesheets = do
