e98de9a66fdafbbdf56d2093357de9442ed1fdd0
Zarak <7254237+zarak@users.noreply.github.com>
2021-01-23 14:03:29 +0500

    Style upload csv button

--- Admin/View/Uploads/Index.hs
+++ Admin/View/Uploads/Index.hs
@@ -6,9 +6,11 @@ data IndexView = IndexView { uploads :: [Upload] }
 
 instance View IndexView where
     html IndexView { .. } = [hsx|
-<div class="pl-4 mb-4">
-    <a href={pathTo NewUploadAction} class="btn btn-primary ml-4">Upload csv</a>
-</div>
+<a href={pathTo NewUploadAction}>
+    <button type="button" class="mb-4 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+      Upload csv
+    </button>
+</a>
 
 <!-- This example requires Tailwind CSS v2.0+ -->
   <div class="flex flex-col">
